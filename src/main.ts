de0e5ac0ef89138187bc70f8c9094d53fe1a9186
Soma Lucz <soma.lucz@diplomatiq.org>
2019-10-04 10:46:24 +0200

    feat: Add support for encoding/decoding Unicode string values
    Partially closes #4.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 export { DefaultBinaryConverter } from './converters/defaultBinaryConverter';
+export { DefaultStringConverter } from './converters/defaultStringConverter';
 export { Base64UrlTransformer } from './interfaces/base64UrlTransformer';
 export { BinaryConverter } from './interfaces/binaryConverter';
+export { Utf8Decoder } from './interfaces/utf8Decoder';
+export { Utf8Encoder } from './interfaces/utf8Encoder';
 export { DefaultBase64UrlTransformer } from './transformers/defaultBase64UrlTransformer';
